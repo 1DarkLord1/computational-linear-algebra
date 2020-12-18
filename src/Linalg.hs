939c9ec756521e7ae4a41e2d6ee718df97735e9f
dword <camm2016@yandex.ru>
2020-12-19 01:12:24 +0300

    some fixes in swapMinor and qrEVShifts

--- src/Linalg.hs
+++ src/Linalg.hs
@@ -283,7 +283,7 @@ doItersQrEVsTridiagonal m eps qk
 
 doItersQrEVsTridiagonalNTimes m cnt qk
     | cnt == 0  = (evs, qk)
-    | otherwise = doItersQrEVsTridiagonal' m' (cnt - 1) q
+    | otherwise = doItersQrEVsTridiagonalNTimes m' (cnt - 1) q
     where
         (gs, r) = qrDecompTridiagonal m
         m'      = trans $ multGivens gs (trans $ toLists r)
@@ -292,37 +292,38 @@ doItersQrEVsTridiagonalNTimes m cnt qk
 
 doItersQrMinEVTridiagonal m eps qk
     | radius < eps = (qk, m)
-    | otherwise    = doItersQrEVTridiagonal' m' eps q
+    | otherwise    = doItersQrMinEVTridiagonal m' eps q
     where
         (gs, r) = qrDecompTridiagonal m
         m'      = trans $ multGivens gs (trans $ toLists r)
         q       = multGivens gs qk
-        lastRow = (head $ reverse $ toLists r) 
-        radius  = foldr (+) 0 lastRow - last lastRow
+        lastRow = last m
+        radius  = foldr (\x acc -> abs x + acc) 0 lastRow - abs (last lastRow)
  
-swapMinor :: Num a => Int -> Matrix a -> Matrix a -> Matrix a
-swapMinor k minor m = m' `add` minor' 
+swapMinor :: Num a => Matrix a -> Matrix a -> Matrix a
+swapMinor minor m = m' `add` minor' 
     where
-        sz     = nrows m
-        m'     = mapPos (\(i, j) x -> if i <= sz - k && j <= sz - k
+        minorSz = nrows minor
+        mSz     = nrows m    
+        m'     = mapPos (\(i, j) x -> if i <= minorSz && j <= minorSz
                                   then 0
                                   else x) m
-        minor' = extendTo k k minor
+        minor' = extendTo 0 mSz mSz minor
 
-qrEVShifts :: (FLoating a, Ord a) => [[a]] -> a -> ([a], Matrix a)
+qrEVShifts :: (Show a, Floating a, Ord a) => [[a]] -> a -> ([a], Matrix a)
 qrEVShifts mt eps = (evs, transpose q)
     where
-        sz = length mt
-        (q, d) = foldl handler (identity $ length mt, fromLists mt) [1..sz]
-        evs    = V.toList $ getDiag $ fromLists d
-        handler k (qk, m) 
-            | k == sz   = (qk, m)
-            | otherwise = (qt `multStd` qk, swapMinor k r' m)
-            where
-                m'      = submatrix 1 (sz - k) 1 (sz - k) m
-                lowerSq = submatrix (sz - 1) sz (sz - 1) sz m'
-                s       = head $ tail $ fst $ 
-                          doItersQrEVsTridiagonalNTimes (toLists lowerSq) 20 (idMatrix 2)
-                m''     = m' `subtr` scaleMatrix s (identity $ nrows m')
-                (qt, r) = first fromLists $ doItersQrMinEVTridiagonal (toLists m'') eps (idMatrix sz)
-                r'      = r `add` scaleMatrix s (identity $ nrows m')
+        sz     = length mt
+        (q, d) = foldl handler (identity (length mt), fromLists mt) [0..sz - 2]
+        evs    = V.toList $ getDiag d
+        handler (qk, m) k 
+               = (qt `multStd` qk, swapMinor r' m)
+               where
+                    m'      = submatrix 1 (sz - k) 1 (sz - k) m
+                    sz'     = nrows m'
+                    lowerSq = submatrix (sz' - 1) sz' (sz' - 1) sz' m'
+                    s       = head $ tail $ fst $ 
+                              doItersQrEVsTridiagonalNTimes (toLists lowerSq) 20 (idMatrix 2)
+                    m''     = m' `subtr` scaleMatrix s (identity sz')
+                    (qt, r) = first fromLists $ doItersQrMinEVTridiagonal (toLists m'') eps (idMatrix sz)
+                    r'      = fromLists r `add` scaleMatrix s (identity sz')
